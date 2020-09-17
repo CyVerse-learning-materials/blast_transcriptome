d192a0a55ae7739118250417e2b701dc0d645416
JasonJWilliamsNY <JasonJWilliamsNY@gmail.com>
2020-09-17 14:44:10 -0400

    add triage issue template

--- conf.py
+++ conf.py
@@ -10,10 +10,10 @@ sys.path.insert(0, os.path.dirname(__file__))
 from misc.cyverse_sphinx_conf import *  # noqa
 
 project = 'Git for Mere Mortals as topic_tutorial'
-copyright = '2019, CyVerse'
+copyright = '2020, CyVerse'
 author = 'Meghan Balk', 'Ramona L. Walls'
-version = '1.0'
-release = '1.0'
+version = '2.0'
+version = '2.0'
 
 epub_title = project
 epub_author = author
